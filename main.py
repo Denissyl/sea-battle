2c89609484b16190d8c023f914ad38e2f314f82b
Konserva <omm20030615>
2020-04-06 19:15:29 +0500

    :sparkles: запрос отправки полей темплейту

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
 import os
 
 from flask import Flask, render_template
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+player1_field = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+player2_field = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+
+
+def main():
+    @app.route('/')
+    def index():
+        fields = pack_fields()
+        return render_template('index.html', fields)
+
+    # @app.route('/1/<buton_id>')
+    # def buttons(button_id):
+    #     redirect()
+
+    app.run()
+
+
+def pack_fields():
+    fields = [player1_field, player2_field]
+    packaged_fields = [[], []]
+    for i in range(len(fields)):
+        for cells in fields[i]:
+            packaged_fields[i].extend(cells)
+
+    return pack_fields()
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 7000))
-    app.run('0.0.0.0', port)
+    main()
