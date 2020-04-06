a13404ed178445d68864317a82a82735d908ba0d
Denis <Denis-syl@yandex.ru>
2020-04-06 22:54:31 +0500

    Переделан шаблон

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
-
 player1_field = [
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -33,24 +32,14 @@ player2_field = [
 def main():
     @app.route('/')
     def index():
-        fields = pack_fields()
-        return render_template('index.html', fields)
+        return render_template('index.html', player1_field=player1_field,
+                               player2_field=player2_field)
 
-    # @app.route('/1/<buton_id>')
+    # @app.route('/button/<button_id>')
     # def buttons(button_id):
     #     redirect()
 
     app.run()
-
-
-def pack_fields():
-    fields = [player1_field, player2_field]
-    packaged_fields = [[], []]
-    for i in range(len(fields)):
-        for cells in fields[i]:
-            packaged_fields[i].extend(cells)
-
-    return pack_fields()
 
 
 if __name__ == '__main__':
