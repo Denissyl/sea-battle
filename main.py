36da347be5531baf57f364cca3bff2ba4df90b92
VoRonex <nikatguy@gmail.com>
2020-04-09 17:45:46 +0500

    created database of players and matches

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask, render_template
 
+from data.db_session import global_init
+
 app = Flask(__name__)
 
 
@@ -11,5 +13,6 @@ def index():
 
 
 if __name__ == '__main__':
+    global_init('db/seabattle.sqlite')
     port = int(os.environ.get('PORT', 7000))
     app.run('0.0.0.0', port)
