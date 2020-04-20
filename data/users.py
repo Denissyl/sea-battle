129dbf8637287f37f4bbd61100efaf8dd237533f
VoRonex <nikatguy@gmail.com>
2020-04-20 18:23:17 +0500

    still falls

--- data/users.py
+++ data/users.py
@@ -13,7 +13,8 @@ class User(SqlAlchemyBase):
     nickname = sqlalchemy.Column(sqlalchemy.String)
     email = sqlalchemy.Column(sqlalchemy.String, unique=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String)
-    fields = orm.relation("Fields", back_populates='user')
+    fields1 = orm.relation("Fields", back_populates='player1', foreign_keys='Fields.player1_id')
+    fields2 = orm.relation("Fields", back_populates='player2', foreign_keys='Fields.player2_id')
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
