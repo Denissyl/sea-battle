8efd6a5c03959f138c59d4392ae6b5728d0e4a18
Denis <Denis-syl@yandex.ru>
2020-04-20 20:28:06 +0500

    Доделана регистрация и вход в аккаунт

--- data/users.py
+++ data/users.py
@@ -1,11 +1,12 @@
 import sqlalchemy
+from flask_login import UserMixin
 from sqlalchemy import orm
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'user'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
